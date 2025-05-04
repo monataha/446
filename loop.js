e2fad88bd68dd925706d60b2c8e9102ee3f858db
monataha <munaadiltaha@gmail.com>
2025-05-04 23:57:00 +0300

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,23 @@
+// for loop
+
+for (let i=0; i<10; i++){
+    document.writeln("مرحبا" + "<br>")
+}
+
+//while loop
+
+let mona = "شاطره";
+let j = 0;
+
+while (j<6){
+    document.writeln(mona + "<br>");
+    j++;
+}
+
+let i = 0;
+let myName = "Monmon";
+
+do {
+    document.writeln(myName + "<br>");
+    i++;
+} while (i < 5);
